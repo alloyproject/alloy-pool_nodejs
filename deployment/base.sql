ee504310b79202da1cb6b39819a031c8cf22a70a
Venthos <venthos@venthos.net>
2018-01-28 17:36:19 -0600

    Discord Webhook integration
    It is now possible to use Discord's Webhook feature to have the pool
    send alerts to a Discord chat room when blocks are mined and/or payouts
    are dispersed.
    
    [ New config options in pool.config ]
     * general.discordWebhook => Set this to the portion of the webhook URL that
       follows https://discordapp.com/api/webhooks/
       e.g. 987245987264223452/mDSj2MSklodmfsdr_fdnk834MNSKFdgn84sund82f4LSDMNF8h42f0bns0f8b4NSDF2s
     * general.discordBlockAnnounce => true/false to enable alerts for mined blocks
     * general.discordPayoutAnnounce => true/false to enable alerts for payouts
    
    See deployment/base.sql for INSERT examples

--- deployment/base.sql
+++ deployment/base.sql
@@ -236,3 +236,6 @@ INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, 
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (5555, 5000, 'Medium-Range Hardware (Up to 160 h/s)', 'pplns', 0, 0);
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (7777, 10000, 'High-End Hardware (Anything else!)', 'pplns', 0, 0);
 INSERT INTO pool.port_config (poolPort, difficulty, portDesc, portType, hidden, `ssl`) VALUES (9000, 20000, 'Claymore SSL', 'pplns', 0, 1);
+INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'discordWebhook', '', 'string', 'Webhook path for Discord messages, this should be the part AFTER https://discordapp.com/api/webhooks/');
+INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'discordBlockAnnounce', 'true', 'bool', 'Announce block finds in Discord');
+INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('general', 'discordPayoutAnnounce', 'true', 'bool', 'Announce payout transactions in Discord');
